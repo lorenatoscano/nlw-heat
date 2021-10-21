ea7a93b38cf632c39afe00953b07fed467a8b0b0
lorenatoscano <lorenatoscano@ufrn.edu.br>
2021-10-21 20:51:45 -0300

    Adiciona conexão com as mensagens enviadas

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -10,7 +10,7 @@ import styles from './App.module.scss';
 export function App() {
   const { user } = useContext(AuthContext);
   return (
-    <main className={styles.contentWrapper}>
+    <main className={`${styles.contentWrapper} ${!!user ? styles.contentSigned : ''}`}>
       <MessageList />
       {!!user ? <SendMessageForm /> : <LoginBox />}
     </main>
