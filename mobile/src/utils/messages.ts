1bb6b51ae73b2275e1a3e339e57d08773cbf1612
lorenatoscano <lorenatoscano@ufrn.edu.br>
2021-10-22 23:24:45 -0300

    Adiciona envio e listagem de mensagens em tempo real

---
+++ mobile/src/utils/messages.ts
@@ -0,0 +1,66 @@
+export const MESSAGES_EXAMPLE = [
+  {
+    id: '1',
+    text: 'Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥',
+    user: {
+      name: 'Dianne Russell',
+      avatar_url: 'https://uifaces.co/our-content/donated/WL1zJdEI.jpg'
+    }
+  },
+  {
+    id: '2',
+    text: 'Esse vai ser simplesmente fantástico! Bora aprender tudo em relação a montagem de APIs GraphQL. Sem contar com as palestras e painéis.',
+    user: {
+      name: 'Guy Hawkins',
+      avatar_url: 'https://randomuser.me/api/portraits/men/43.jpg'
+    }
+  },
+  {
+    id: '3',
+    text: 'Sem dúvida as palestras vão ser úteis para a minha carreira e para a de muitos 😍 #gorocket',
+    user: {
+      name: 'Eleanor Pena',
+      avatar_url: 'https://uifaces.co/our-content/donated/hf5SZ6bM.jpg'
+    }
+  },
+  {
+    id: '4',
+    text: 'Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥',
+    user: {
+      name: 'Dianne Russell',
+      avatar_url: 'https://uifaces.co/our-content/donated/WL1zJdEI.jpg'
+    }
+  },
+  {
+    id: '5',
+    text: 'Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥',
+    user: {
+      name: 'Linda Russell',
+      avatar_url: 'https://uifaces.co/our-content/donated/cQkGbpRO.jpg'
+    }
+  },
+  {
+    id: '6',
+    text: 'Esse vai ser simplesmente fantástico! Bora aprender tudo em relação a montagem de APIs GraphQL. Sem contar com as palestras e painéis.',
+    user: {
+      name: 'Guy Hawkins',
+      avatar_url: 'https://uifaces.co/our-content/donated/vIqzOHXj.jpg'
+    }
+  },
+  {
+    id: '7',
+    text: 'Sem dúvida as palestras vão ser úteis para a minha carreira e para a de muitos 😍 #gorocket',
+    user: {
+      name: 'Eleanor Pena',
+      avatar_url: 'https://uifaces.co/our-content/donated/7tBovADy.jpg'
+    }
+  },
+  {
+    id: '8',
+    text: 'Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥',
+    user: {
+      name: 'Michele Fernandes',
+      avatar_url: 'https://uifaces.co/our-content/donated/XdLjsJX_.jpg'
+    }
+  },
+];
