7db81a9fdf3133d1ee2df077c0bd54844724d9ad
lorenatoscano <lorenatoscano@ufrn.edu.br>
2021-10-22 22:19:23 -0300

    Adiciona autenticação no aplicativo

--- mobile/src/components/Header/index.tsx
+++ mobile/src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { useAuth } from '../../hooks/auth';
 import { View, Text, TouchableOpacity } from 'react-native';
 
 import { UserPhoto } from '../UserPhoto';
@@ -9,16 +9,22 @@ import { styles } from './styles';
 import LogoSvg from '../../assets/logo.svg';
 
 
+
 export function Header() {
+  const { user, signOut } = useAuth();
   return (
     <View style={styles.container}>
       <LogoSvg />
 
       <View style={styles.logoutButton}>
-        <TouchableOpacity>
-          <Text style={styles.logoutText}>Sair</Text>
-        </TouchableOpacity>
-        <UserPhoto imageUri="https://github.com/lorenatoscano.png" />
+        {
+          user &&
+          <TouchableOpacity onPress={signOut}>
+            <Text style={styles.logoutText}>Sair</Text>
+          </TouchableOpacity>
+        }
+
+        <UserPhoto imageUri={user?.avatar_url} />
       </View>
     </View>
   );
--- mobile/src/components/MessageList/index.tsx
+++ mobile/src/components/MessageList/index.tsx
@@ -1,27 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ScrollView } from 'react-native';
-import { Message } from '../Message';
+import { api } from '../../services/api';
+import { Message, MessageProps } from '../Message';
 
 import { styles } from './styles';
 
 export function MessageList() {
-  const message = {
-    id: '1',
-    text: 'Texto da mensagem',
-    user: {
-      name: 'Lorena',
-      avatar_url: 'https://github.com/lorenatoscano.png'
+  const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([]);
+
+  useEffect(() => {
+    async function fetchMessages() {
+      const messagesResponse = await api.get<MessageProps[]>('/messages/last3');
+      setCurrentMessages(messagesResponse.data);
     }
-  }
+
+    fetchMessages();
+  }, []);
   return (
     <ScrollView
       style={styles.container}
       contentContainerStyle={styles.content}
       keyboardShouldPersistTaps="never"
     >
-      <Message data={message} />
-      <Message data={message} />
-      <Message data={message} />
+      {currentMessages.map((message) => <Message key={message.id} data={message} />)}
+
     </ScrollView>
   );
 }
